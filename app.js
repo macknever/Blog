ce289d289b49ea8de914cbe3db03c5e8893a55e6
Lawrence <macknever@gmail.com>
2021-05-21 13:12:15 -0700

    stop using .env to store the clientID and client secret

--- app.js
+++ app.js
@@ -94,8 +94,8 @@ passport.deserializeUser(function (id, done) {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
+      clientID: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
       callbackURL: "http://localhost:3000/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
